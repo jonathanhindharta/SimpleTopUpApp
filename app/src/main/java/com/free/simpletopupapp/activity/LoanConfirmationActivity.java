0f01e53f241e5d0a0765f8916053d2037b7fb169
Jonathan <jonathan@ekomoditi.id>
2020-02-26 23:57:04 +0700

    update LoanConfirmation

---
+++ app/src/main/java/com/free/simpletopupapp/activity/LoanConfirmationActivity.java
@@ -0,0 +1,15 @@
+package com.free.simpletopupapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.free.simpletopupapp.R;
+
+public class LoanConfirmationActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loan_confirmation);
+    }
+}
